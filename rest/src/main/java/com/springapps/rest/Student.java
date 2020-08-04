73a15b1505787b1f29ec20687d1a0dd15d9a1dcc
VJ1224 <vanshjain1224@gmail.com>
2020-08-04 21:31:21 +0530

    Started REST API

---
+++ rest/src/main/java/com/springapps/rest/Student.java
@@ -0,0 +1,37 @@
+package com.springapps.rest;
+
+public class Student {
+    private int uid;
+    private String name;
+    private int rollNo;
+
+    public Student(int uid, String name, int rollNo) {
+        this.uid = uid;
+        this.name = name;
+        this.rollNo = rollNo;
+    }
+
+    public int getUid() {
+        return uid;
+    }
+
+    public void setUid(int uid) {
+        this.uid = uid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getRollNo() {
+        return rollNo;
+    }
+
+    public void setRollNo(int rollNo) {
+        this.rollNo = rollNo;
+    }
+}
