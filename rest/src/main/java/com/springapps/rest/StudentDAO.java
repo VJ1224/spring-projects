e161fa584afa59df87dd30024fe14afc210119e9
VJ1224 <vanshjain1224@gmail.com>
2020-08-07 20:51:36 +0530

    Fixed redundancy

--- rest/src/main/java/com/springapps/rest/StudentDAO.java
+++ rest/src/main/java/com/springapps/rest/StudentDAO.java
@@ -4,19 +4,19 @@ import java.util.List;
 import javax.sql.DataSource;
 
 public interface StudentDAO {
-    public void setDataSource(DataSource ds);
+    void setDataSource(DataSource ds);
 
-    public List<Student> listStudents();
+    List<Student> listStudents();
 
-    public Student getStudentByUID(int uid);
+    Student getStudentByUID(int uid);
 
-    public List<Student> getStudentByRollNumber(int rollNo);
+    List<Student> getStudentByRollNumber(int rollNo);
 
-    public List<Student> getStudentByCourse(String course);
+    List<Student> getStudentByCourse(String course);
 
-    public Student addStudent(Student student);
+    Student addStudent(Student student);
 
-    public Student deleteStudent(int uid);
+    Student deleteStudent(int uid);
 
-    public Student updateStudent(Student student);
+    Student updateStudent(Student student);
 }
